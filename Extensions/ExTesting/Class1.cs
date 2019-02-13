fbd63e689dfd99882e576dba21a78c055151ddd0
Tobias <tobis-stroedicke@gmx.de>
2019-02-13 16:05:41 +0100

    add Project

---
+++ Extensions/ExTesting/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String;
+using List;
+
+namespace ExTesting
+{
+    [TestClass]
+    public class StringTest
+    {
+        [TestMethod]
+        public void StringTester1()
+        {
+            string s = "Hallabbakko";
+            s = s.RemoveChars('a', 'o');
+
+            Assert.AreEqual(s, "Hllbbkk");
+        }
+    }
+
+    [TestClass]
+    public class IntTest
+    {
+
+    }
+
+    [TestClass]
+    public class ListTest
+    {
+        [TestMethod]
+        public void ListTester1()
+        {
+            List<int> intList = new List<int> { 1, 2, 3, 4, 5 };
+            int last = intList.GetItemFromBack(-1);
+
+            Assert.AreEqual(5, last);
+        }
+    }
+}
